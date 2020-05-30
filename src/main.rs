b667d8809635acdf6bb0b49a9f5edc09d299c13a
Théo <theo.huchard@gmail.com>
2020-05-30 20:56:49 +0200

    Optimisation test 2

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,34 @@ use std::process::{Command, Stdio};
 use walkdir::WalkDir;
 
 fn main(){
-    let mut p: Vec<String> = vec![];
-    for arg in env::args().skip(1) {
-        for e in WalkDir::new(arg).into_iter().filter_map(Result::ok).filter(|e| !e.file_type().is_dir()) {
-            p.push(format!("{}", e.path().display()));
-        }
-    }
-
     let mut hashcat = String::from("");
-    p.sort();
-    for arg in p {
-        let mut output = Command::new("md5sum")
-                    .arg(arg)
-                    .output()
-                    .expect("failed to execute process");
+    let mut output;
 
-        for e in String::from_utf8_lossy(&output.stdout).to_string().split_whitespace().next(){
-            hashcat = format!("{}{}", hashcat, e.to_string());
+    for arg in env::args().skip(1) {        
+        for x in WalkDir::new(arg).into_iter().filter_map(Result::ok).filter(|e| !e.file_type().is_dir()) {
+            output = Command::new("md5sum")
+            .arg(x.path().display().to_string())
+            .output()
+            .expect("failed to execute process");
+            for e in String::from_utf8_lossy(&output.stdout).to_string().split_whitespace().next(){
+                hashcat = format!("{}{}", hashcat, e.to_string());
+            }
         }
     }
-    
-    let mut output = Command::new("echo")
-                    .arg(hashcat)
-                    .stdout(Stdio::piped())
-                    .spawn()
-                    .expect("failed to execute process");
 
-    let mut output2 = Command::new("md5sum")
-                    .stdin(output.stdout.unwrap())
-                    .output()
-                    .expect("failed to execute process");
-
-    let mut output3 = String::from_utf8_lossy(&output2.stdout).to_string();
-    
-    println!("{}", output3);
+    print!("{}",
+        String::from_utf8_lossy(
+        &Command::new("md5sum")
+        .stdin(
+            Command::new("echo")
+            .arg(hashcat)
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("failed to execute process")
+            .stdout.unwrap()
+        )
+        .output()
+        .expect("failed to execute process").stdout)
+        .to_string()
+    );
 }
