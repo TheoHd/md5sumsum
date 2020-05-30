97552a21b5686a9ee9c8e11eea1f497d8163f8fa
Théo <theo.huchard@gmail.com>
2020-05-30 21:36:45 +0200

    Optimisation test 4

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
 use std::process::{Command, Stdio};
-use jwalk::WalkDir;
+use walkdir::WalkDir;
 
 fn main(){
     let mut hashcat = String::from("");
     let mut output;
 
-    for arg in env::args().skip(1) {        
+    for arg in env::args().skip(1) {
         for x in WalkDir::new(arg).into_iter().filter_map(Result::ok).filter(|e| !e.file_type().is_dir()) {
             output = Command::new("md5sum")
             .arg(x.path().display().to_string())
@@ -20,17 +20,17 @@ fn main(){
 
     print!("{}",
         String::from_utf8_lossy(
-        &Command::new("md5sum")
-        .stdin(
-            Command::new("echo")
-            .arg(hashcat)
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("failed to execute process")
-            .stdout.unwrap()
-        )
-        .output()
-        .expect("failed to execute process").stdout)
-        .to_string()
+            &Command::new("md5sum")
+            .stdin(
+                Command::new("echo")
+                .arg(hashcat)
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("failed to execute process")
+                .stdout.unwrap()
+            )
+            .output()
+            .expect("failed to execute process").stdout
+        ).to_string()
     );
 }
