ec80104154a52434b186faf569c176a4ef659af7
Théo <theo.huchard@gmail.com>
2020-06-02 13:09:15 +0200

    Test performance 6: Changed library and added a continue in the for loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,89 +1,47 @@
 #![feature(test)]
 
 use std::env;
-use std::process::{Command, Stdio};
 use walkdir::WalkDir;
-use md5;
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
 
 extern crate test;
 
 pub fn walkdir_in_args() {
     for arg in env::args().skip(1) {
-        for x in WalkDir::new(arg).into_iter().filter_map(Result::ok).filter(|e| !e.file_type().is_dir()) {
-        }
-    }
-}
-
-pub fn md5sum_with_no_hashcat() {
-    let mut output;
-    for arg in env::args().skip(1) {
-        for x in WalkDir::new(arg).into_iter().filter_map(Result::ok).filter(|e| !e.file_type().is_dir()) {
-            output = Command::new("md5sum")
-            .arg(x.path().display().to_string())
-            .output()
-            .expect("failed to execute process");
-        }
-    }
-}
-
-pub fn md5sum_with_hashcat() {
-    let mut hashcat = String::from("");
-    let mut output;
-    for arg in env::args().skip(1) {
-        for x in WalkDir::new(arg).into_iter().filter_map(Result::ok).filter(|e| !e.file_type().is_dir()) {
-            output = Command::new("md5sum")
-            .arg(x.path().display().to_string())
-            .output()
-            .expect("failed to execute process");
-            for e in String::from_utf8_lossy(&output.stdout).to_string().split_whitespace().next(){
-                hashcat = format!("{}{}", hashcat, e.to_string());
-            }
+        for _x in WalkDir::new(arg).into_iter().filter_map(|e| e.ok()) {
         }
     }
 }
 
 pub fn get_hashcat() -> String{
     let mut hashcat = String::from("");
-    //let mut output;
-    let mut digest;
     for arg in env::args().skip(1) {
-        for x in WalkDir::new(arg).into_iter().filter_map(Result::ok).filter(|e| !e.file_type().is_dir()) {
-            digest = md5::compute(String::from(x.path().to_string_lossy()));
-            //output = Command::new("md5sum")
-            //    .arg(String::from(x.path().to_string_lossy()))
-            //    .output()
-            //    .unwrap();
-            //hashcat += String::from_utf8_lossy(&output.stdout).to_string().split(" ").collect::<Vec<&str>>()[0];
-            hashcat += format!("{:x}",digest).to_string().split(" ").collect::<Vec<&str>>()[0];
+        for x in WalkDir::new(arg).into_iter().filter_map(|e| e.ok()) {
+            if !x.file_type().is_file() {
+                continue;
+            }
+            let mut digest = Md5::new();
+            digest.input_str(&x.path().display().to_string());
+            hashcat += digest.result_str().split(" ").collect::<Vec<&str>>()[0];
         }
     }
     hashcat
 }
 
-pub fn echo_cmd(hashcat: &mut String){
-    md5::compute(hashcat);
-    //Command::new("md5sum")
-    //      .arg(hashcat)
-            //.output()
-            //.unwrap();
+pub fn echo_cmd(hashcat: &mut String) {
+    let mut digest = Md5::new();
+    digest.input_str(hashcat);
 }
 
 pub fn final_print(hashcat: &mut String) {
+    let mut digest = Md5::new();
+    let mut digest2 = Md5::new();
+    digest2.input_str(hashcat);
+    digest.input_str(&digest2.result_str());
     print!("{}",
-        format!("{:x}",md5::compute(format!("{:x}",md5::compute(hashcat))))
-        //String::from_utf8_lossy(
-            //&Command::new("md5sum")
-            //.stdin(
-                //Command::new("echo")
-                //.arg(hashcat)
-                //.stdout(Stdio::piped())
-                //.spawn()
-                //.expect("failed to execute process")
-                //.stdout.unwrap()
-            //)
-            //.output()
-            //.expect("failed to execute process").stdout
-        //).to_string()
+        digest.result_str()
     );
 }
 
@@ -95,16 +53,6 @@ mod tests {
     #[bench]
     fn test_walkdir_in_args(b: &mut Bencher){
         b.iter(|| walkdir_in_args());
-    }
-
-    #[bench]
-    fn test_md5sum_with_no_hashcat(b: &mut Bencher){
-        b.iter(|| md5sum_with_no_hashcat());
-    }
-
-    #[bench]
-    fn test_md5sum_with_hashcat(b: &mut Bencher){
-        b.iter(|| md5sum_with_hashcat());
     }
     
     #[bench]
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 use std::env;
 use walkdir::WalkDir;
-use md5;
-
-fn get_hashcat() -> String{
-    let mut hashcat = String::from("");
-    let mut digest;
-    for arg in env::args().skip(1) {
-        for x in WalkDir::new(arg).into_iter().filter_map(Result::ok).filter(|e| !e.file_type().is_dir()) {
-            digest = md5::compute(String::from(x.path().to_string_lossy()));
-            hashcat += format!("{:x}",digest).to_string().split(" ").collect::<Vec<&str>>()[0];
-        }
-    }
-    hashcat
-}
+use crypto::md5::Md5;
+use crypto::digest::Digest;
 
 fn main(){
-    let hashcat = get_hashcat();
-    print!("{}",format!("{:x}",md5::compute(format!("{:x}",md5::compute(hashcat)))));
+    let mut hashcat = String::from("");
+
+    for arg in env::args().skip(1) {
+        for x in WalkDir::new(arg).into_iter().filter_map(|e| e.ok()) {
+            if !x.file_type().is_file() {
+                continue;
+            }
+            let mut digest = Md5::new();
+            digest.input_str(&x.path().display().to_string());
+            hashcat += digest.result_str().split(" ").collect::<Vec<&str>>()[0];
+        }
+    }
+
+    let mut digest = Md5::new();
+    let mut digest2 = Md5::new();
+    digest.input_str(&hashcat);
+    digest2.input_str(&digest.result_str());
+    print!("{}\n",
+        digest2.result_str()
+    );
 }
